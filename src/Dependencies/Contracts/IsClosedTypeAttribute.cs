47859a0612a96ee96430a4bfb3daeb3d8d9422d4
Fredric Silberberg <frsilb@microsoft.com>
2026-09-09 13:00:07 -0700

    Add backing attributes

---
+++ src/Dependencies/Contracts/IsClosedTypeAttribute.cs
@@ -0,0 +1,49 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+// Copied from:
+// https://github.com/dotnet/runtime/blob/80a8cdbdb9c36ccab97374afcb2f0491ae58a94d/src/libraries/System.Private.CoreLib/src/System/Runtime/CompilerServices/IsClosedTypeAttribute.cs
+
+#if NET11_0_OR_GREATER
+
+
+using System.Runtime.CompilerServices;
+
+#pragma warning disable RS0016 // Add public types and members to the declared API (this is a supporting forwarder for an internal polyfill API)
+[assembly: TypeForwardedTo(typeof(IsClosedTypeAttribute))]
+#pragma warning restore RS0016 // Add public types and members to the declared API
+
+#else
+
+#nullable enable
+
+using System.ComponentModel;
+
+namespace System.Runtime.CompilerServices
+{
+    /// <summary>
+    /// Reserved for use by a compiler for tracking metadata.
+    /// This attribute should not be used by developers in source code.
+    /// </summary>
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    [AttributeUsage(AttributeTargets.Class, Inherited = false)]
+    internal sealed class IsClosedTypeAttribute : Attribute
+    {
+        private Type[] _derivedTypes = Type.EmptyTypes;
+
+        /// <summary>Initializes the attribute.</summary>
+        public IsClosedTypeAttribute()
+        {
+        }
+
+        /// <summary>Gets or sets the derived types of the closed type.</summary>
+        /// <value>An array of the derived types of the closed type. A <see langword="null" /> value is normalized to an empty array.</value>
+        public Type[] DerivedTypes
+        {
+            get => _derivedTypes;
+            set => _derivedTypes = value ?? Type.EmptyTypes;
+        }
+    }
+}
+#endif
---
+++ src/Dependencies/Contracts/UnionAttribute.cs
@@ -0,0 +1,37 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+// Copied from:
+// https://github.com/dotnet/runtime/blob/80a8cdbdb9c36ccab97374afcb2f0491ae58a94d/src/libraries/System.Private.CoreLib/src/System/Runtime/CompilerServices/IUnion.cs
+
+#if NET11_0_OR_GREATER
+
+using System.Runtime.CompilerServices;
+
+#pragma warning disable RS0016 // Add public types and members to the declared API (this is a supporting forwarder for an internal polyfill API)
+[assembly: TypeForwardedTo(typeof(UnionAttribute))]
+#pragma warning restore RS0016 // Add public types and members to the declared API
+
+#else
+
+namespace System.Runtime.CompilerServices
+{
+    /// <summary>
+    /// Indicates that a class or struct is a union type, enabling compiler support for union behaviors.
+    /// </summary>
+    /// <remarks>
+    /// <para>
+    /// Any class or struct annotated with this attribute is recognized by the C# compiler as a union type.
+    /// Union types may support behaviors such as implicit conversions from case types, pattern matching
+    /// that unwraps the union's contents, and switch exhaustiveness checking.
+    /// </para>
+    /// </remarks>
+    /// <seealso cref="IUnion" />
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct, AllowMultiple = false, Inherited = false)]
+    internal sealed class UnionAttribute : Attribute
+    {
+    }
+}
+
+#endif
